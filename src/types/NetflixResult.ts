a1788afb1e771e52b366365e7759b9dcbfcdae8c
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2021-02-06 18:47:19 -0300

    add neflix result tupple

---
+++ src/types/NetflixResult.ts
@@ -0,0 +1,16 @@
+import { ShowType } from './ShowType';
+
+export type NetflixResult = [
+    string,
+    ShowType,
+    string,
+    string,
+    string,
+    string,
+    Date,
+    number,
+    string,
+    string,
+    string,
+    string
+];
