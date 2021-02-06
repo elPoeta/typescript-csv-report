4cfbc014e6f1be5fc304cc35aba900ece6b7d7cc
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2021-02-06 18:27:01 -0300

    build

---
+++ build/types/ShowType.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ShowType = void 0;
+var ShowType;
+(function (ShowType) {
+    ShowType["MOVIE"] = "Movie";
+    ShowType["TV_SHOW"] = "TV Show";
+})(ShowType = exports.ShowType || (exports.ShowType = {}));
