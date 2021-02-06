0e3fdb2d250c48057657e2b58f3c582ccd456be6
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2021-02-05 23:01:04 -0300

    create singleton FileOperation

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import { FileOperation } from './utils/FileOperation';
 
-const fileOperation = new FileOperation('netflix.csv');
 const read = async () => {
-    const data = await fileOperation.read();
+    const data = await FileOperation.getInstance().read('netflix.csv');
     if (data === 'ENOENT') {
         console.log('ENOENT: no such file or directory, open')
         return;
--- src/utils/FileOperation.ts
+++ src/utils/FileOperation.ts
@@ -3,12 +3,17 @@ import path from 'path';
 
 export class FileOperation {
     private baseDir: string = path.resolve(__dirname, '../../csv');
-    constructor(public fileName: string) { }
 
-    async read(): Promise<string> {
+    static getInstance() {
+        return new FileOperation();
+    }
+
+    constructor() { }
+
+    async read(fileName: string): Promise<string> {
         try {
-            await fs.open(`${this.baseDir}/${this.fileName}`, 'r');
-            const data = await fs.readFile(`${this.baseDir}/${this.fileName}`, 'utf-8');
+            await fs.open(`${this.baseDir}/${fileName}`, 'r');
+            const data = await fs.readFile(`${this.baseDir}/${fileName}`, 'utf-8');
             return data;
         } catch (error) {
             return error.code;
